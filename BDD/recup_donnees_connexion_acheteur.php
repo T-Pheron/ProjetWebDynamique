bfdfbc343070e1f0ce066bb2ac204fcd03cabd20
T-Pheron <theric.pheron@edu.ece.fr>
2021-05-29 17:38:36 +0200

    Mise en page - 29/05/21

--- BDD/recup_donnees_connexion_acheteur.php
+++ BDD/recup_donnees_connexion_acheteur.php
@@ -1,4 +1,6 @@
 <?php
+var_dump($_POST);
+
  //connexion à la base de données
  try{
  $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
@@ -11,22 +13,19 @@
       $mdp = $_POST['mdp'];
    
       $req = $bdd->prepare('SELECT mail, mdp FROM acheteur WHERE mail = :mail AND mdp = :mdp');
-      $req->execute(array(':mail' => $mail_bdd, ':mdp' => $mdp_bdd ));
+      $req->execute(array(':mail' => $mail, ':mdp' => $mdp ));
       $res = $req->fetch();
-
-      echo $mail_bdd;
-      echo $mdp_bdd;
        
       if($res)
       {
-        if($mdp_bdd === $res['mdp'] && $mail_bdd === $res['mail'])
+        if($mdp === $res['mdp'] && $mail === $res['mail'])
         {
-            $message='cest oke ca a bien ete trouve mafia';
-            header('Location: ../PageAcceuil.html');
+            echo 'cest oke ca a bien ete trouve mafia';
+            header('Location: ../PageAccueil_Acheteur.html');
         }
         else
         {
-            $message='cest pas encore ca mafia';
+            echo 'cest pas encore ca mafia';
             //header('location:aboutus.php');
         }
       }
@@ -42,23 +41,3 @@ catch (Exception $e)
 {
         die('Erreur : ' . $e->getMessage());
 }
-//}
-  
- ?>
-
-
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>vérification</title>
-</head>
-<body>
-    <h2>Est ce que la connexion a fonctionné ?</h2>
-    <p><?php echo $message;
-         echo $mail_bdd;
-         echo $mdp_bdd;?></p>
-</body>
-</html>
